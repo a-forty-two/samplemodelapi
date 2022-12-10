73cc1c265d1334e567c0438e0afc2c9c06491430
Shantanu Pandey <shantanu.pandey@live.in>
2022-12-10 14:31:18 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from joblib import load
 import pandas as pd
-app = Flask(__app__)
+app = Flask(__name__)
 model = load('model.joblib') 
 @app.route('/')
 def welcome():
   return "Welcome to our model!"
 
-@app.route('/<r>/<a>')
+@app.route('/predict/<r>/<a>')
 def predict(r,a):
+  r = float(r)
+  a = float(a)
   df = pd.DataFrame({
       "radius_mean": [r],
       "area_mean":[a]
   })
-  p = model.predict(df)
-  return p
+  p = str(model.predict(df)[0])
+  p = 'Malignant' if p=='1' else 'Benign'
+  return {'result':p}
 
-
-
-
+app.run()
